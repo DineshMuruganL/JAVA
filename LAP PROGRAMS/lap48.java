0006a7872fade4ca697ac1e8c7c610d92728b7fe
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:36:59 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap48.java
@@ -0,0 +1,18 @@
+//Java interface
+interface printable{  
+		void print();  
+		}  
+public class lap48 implements printable {
+	
+	public void print(){System.out.println("M.Dineshkumar");}
+	
+	public static void main(String[] args) {
+		  
+			  
+			lap48 obj = new lap48(); 
+			obj.print();  
+			 }  
+
+	}
+
+
