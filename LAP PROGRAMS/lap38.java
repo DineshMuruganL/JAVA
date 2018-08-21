90c575fb8c84b77723b3f3958da627e271a0b35e
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:25:32 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap38.java
@@ -0,0 +1,17 @@
+//Java final class
+
+	final class Bike{}  
+	  
+ public	class lap38 extends Bike{  
+	  void run(){System.out.println("running safely with 100kmph");}  
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			    
+			  lap38 honda= new lap38();  
+			  honda.run();  
+			  }  
+
+	}
+
+
