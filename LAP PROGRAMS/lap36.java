c83de2c941125aaf378947884eb9b1c1d7fb20e0
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:24:26 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap36.java
@@ -0,0 +1,17 @@
+//Java final variable
+public class lap36 {
+	final int speedlimit;//final variable  
+	 void run(){  
+	  //speedlimit=400;  
+	//System.out.print(speedlimit);
+	 }  
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 lap36 obj=new  lap36();  
+			 obj.run();  
+			 }  
+			}//end of class  
+
+	
