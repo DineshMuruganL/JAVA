cf3d0162d6d551af514e19e8751b8b746d960f3a
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:26:59 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap39.java
@@ -0,0 +1,13 @@
+//Is final method inherited?
+	class Bike{  
+		  final void run(){System.out.println("working...");}  
+		}  
+ public	class lap39 extends Bike{  
+
+	public static void main(String[] args) {
+			    new lap39().run();  
+		   }  
+
+	}
+
+
