1a6fee2b253959b880dcbf44484428dd5301ff9b
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:30:55 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap42.java
@@ -0,0 +1,12 @@
+//static binding
+public class lap42 {
+	 private void eat(){System.out.println("program is running...");}  
+	
+	public static void main(String[] args) {
+			  
+			  lap42 d1=new lap42();  
+			  d1.eat();  
+			 }  
+			}  
+
+	
