09e39d185ecc891e519a3539d27d70b081c008cb
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:33:43 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap44.java
@@ -0,0 +1,14 @@
+//Simple example of java instanceof
+ class Animal {}
+  
+public	class lap44 extends Animal{//Dog inherits Animal  
+
+	public static void main(String[] args) {
+			  
+			 lap44 d=new lap44();  
+			 System.out.println(d instanceof Animal);//true  
+			 }  
+
+	}
+
+
