9fc43379f4521c9b191a45e981132462a6c47481
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:15:35 +0000

    Add files via upload

---
+++ LAP PROGRAMS/lap30.java
@@ -0,0 +1,15 @@
+//Understanding the problem without method overriding
+public class lap30 {
+		  void run(){System.out.println("Vehicle is running");}  
+		}  
+			class Bike extends lap30{  
+			    
+			  public static void main(String args[]){  
+		  Bike obj = new Bike();  
+			  obj.run();  
+			  }  
+
+
+	}
+
+
