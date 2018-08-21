6ee4ea7db377ef8400690a9b4330a52d1858d76f
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-21 13:04:34 +0000

    Add files via upload

---
+++ lap26.java
@@ -0,0 +1,17 @@
+//Example of this keyword that you return as a statement from the method
+public class lap26 {
+		lap26 getA(){  
+		return this;  
+		}  
+		void msg(){System.out.println("I M DINESH");}  
+		}  
+		  
+		class Test1{  
+	
+	public static void main(String[] args) {
+			new lap26().getA().msg();  
+			}  
+
+	}
+
+
