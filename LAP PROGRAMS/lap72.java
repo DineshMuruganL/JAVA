6272e2264b5a872a9e6d366d6b2cddbd1daa8e4c
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-22 16:22:08 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap72.java
@@ -0,0 +1,11 @@
+//String Concatenation by +
+	
+package lap;
+
+public class lap72 {
+		 public static void main(String args[]){  
+		   String s="dinesh"+" kumar";  
+		   System.out.println(s);  
+		 }  
+
+}
