2824b1cdfac549274677cc8ff8b707a6c3911554
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-22 16:22:45 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap74.java
@@ -0,0 +1,11 @@
+//substring
+	
+package lap;
+
+public class lap74 {
+		 public static void main(String args[]){  
+		   String s="dinesh kumar";  
+		   System.out.println(s.substring(6));
+		   System.out.println(s.substring(0,6));  
+		 }
+}
