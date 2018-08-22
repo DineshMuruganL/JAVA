c129fc0e12be76d0cc50d8bb2d97e77e8c463271
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-22 16:26:25 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap87.java
@@ -0,0 +1,12 @@
+
+//reverse()
+	package lap;
+
+public class lap87 {
+		public static void main(String args[]){  
+		StringBuffer sb=new StringBuffer("dinesh");  
+		sb.reverse();  
+		System.out.println(sb);
+		}  
+
+}
