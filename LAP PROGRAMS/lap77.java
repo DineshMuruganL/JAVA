9b2171bb4360f13cef03a49159467e9bd65d638e
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-22 16:23:27 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap77.java
@@ -0,0 +1,12 @@
+	//startsWith() and endsWith()
+
+package lap;
+
+public class lap77 {
+	public static void main(String ar[]) {
+
+		String s="dinesh";  
+		 System.out.println(s.startsWith("di")); 
+		 System.out.println(s.endsWith("k")); 
+}
+}
