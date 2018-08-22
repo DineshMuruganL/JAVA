77202986994ef92da3076c42337fac1105246ba5
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-22 16:23:42 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap78.java
@@ -0,0 +1,12 @@
+//charAt() 
+	
+package lap;
+
+public class lap78 {
+	public static void main(String ar[]) {
+
+		String s="dinesh";  
+		System.out.println(s.charAt(0)); 
+		System.out.println(s.charAt(3));  
+}
+}
