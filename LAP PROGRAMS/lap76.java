3557191ffb69664171228e7f228b55c8584b9422
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-22 16:23:13 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap76.java
@@ -0,0 +1,11 @@
+//trim() 
+	
+package lap;
+
+public class lap76 {
+	public static void main(String ar[]) {
+		String s="  dinesh ";  
+		System.out.println(s);    
+		System.out.println(s.trim());  
+	}
+}
