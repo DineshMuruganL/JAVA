6ac83e57a476b620696103e054107cfccd518d47
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-23 15:10:26 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap53a.java
@@ -0,0 +1,14 @@
+package lap53a;
+import lap53.*;
+public	class lap53a{  
+	  public static void main(String args[]){  
+		   lap53 obj = new lap53();//Compile Time Error  
+	   obj.msg();//Compile Time Error  
+		  }  
+		}  
+
+
+
+
+
+
