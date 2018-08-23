1ff947b1f75a38d02d2add8c2922d702b94ee14b
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-23 15:13:33 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap56a.java
@@ -0,0 +1,12 @@
+package din;
+
+public class lap56a {
+			public static void main(String[] args){  
+			lap56 s=new lap56();  
+			s.setName("dineshkumar");  
+		System.out.println(s.getName());  
+			}  
+
+	}
+
+
