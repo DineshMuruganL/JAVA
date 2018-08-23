82fcfa51a77f72d307d896621f359412d7519593
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-23 15:15:43 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap60.java
@@ -0,0 +1,21 @@
+//Multidimensional java array
+		
+public class lap60 {
+
+			public static void main(String args[]){  
+			  
+			//declaring and initializing 2D array  
+		int arr[][]={{1,2,0},{5,4,6},{0,1,0}};  
+			  
+		//printing 2D array  
+			for(int i=0;i<3;i++){  
+			 for(int j=0;j<3;j++){  
+		  System.out.print(arr[i][j]+" ");  
+			 }  
+			 System.out.println();  
+			}  
+			  
+
+	}
+
+}
