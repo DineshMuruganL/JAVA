e81f5ee46d7187023cd961662f952d0a660f34f6
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-23 15:11:27 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap54a.java
@@ -0,0 +1,13 @@
+package lap54a;
+import lap54.*;  
+
+public class lap54a extends lap54{
+
+		 public static void main(String args[]){  
+		   lap54a obj = new lap54a();  
+	   obj.msg();  
+		  }  
+}  
+
+
+
