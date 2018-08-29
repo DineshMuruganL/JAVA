378e1cb494a06ec77d314e54316f96cd9a9809f1
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-29 14:57:10 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap147.java
@@ -0,0 +1,15 @@
+//reading data from keyboard by InputStreamReader and BufferdReader class:
+import java.io.*;  
+
+public class lap147 {
+	public static void main(String args[])throws Exception{  
+	  
+		InputStreamReader r=new InputStreamReader(System.in);  
+		BufferedReader br=new BufferedReader(r);  
+		  
+		System.out.println("Enter your name");  
+		String name=br.readLine();  
+		System.out.println("Welcome "+name);  
+		 }  
+
+}
