f333ab57c77d669e79c4da3541b21a6d7f3c7327
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-29 14:34:33 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap107.java
@@ -0,0 +1,20 @@
+//throws 
+	import java.io.IOException;  
+	class lap107{  
+	  void m()throws IOException{  
+	    throw new IOException("device error");//checked exception  
+	  }  
+	  void n()throws IOException{  
+	    m();  
+	  }  
+	  void p(){  
+	   try{  
+	    n();  
+	   }catch(Exception e){System.out.println("exception handled");}  
+	  }  
+	  public static void main(String args[]){  
+	   lap107 obj=new lap107();  
+	   obj.p();  
+	   System.out.println("normal flow...");  
+	  }  
+	}  
