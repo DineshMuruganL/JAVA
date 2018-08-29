5a4a6d903e551d0a889a81a268babb4bddd3f295
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-29 14:55:43 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap142.java
@@ -0,0 +1,18 @@
+//BufferedOutputStream class:
+import java.io.*;  
+	
+public class lap142 {
+	 public static void main(String args[])throws Exception{  
+	   FileOutputStream fout=new FileOutputStream("f1.txt");  
+	   BufferedOutputStream bout=new BufferedOutputStream(fout);  
+	   String s="murugan is my father";  
+	   byte b[]=s.getBytes();  
+	   bout.write(b);  
+  
+	   bout.flush();  
+	   bout.close();  
+	   fout.close();  
+	   System.out.println("success");  
+	 }  
+
+}
