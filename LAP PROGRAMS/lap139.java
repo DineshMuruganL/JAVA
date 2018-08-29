5f75519146de9488bb05b890590e1f579476af4c
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-08-29 14:54:25 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap139.java
@@ -0,0 +1,19 @@
+//ByteArrayOutputStream Example
+	import java.io.*;  
+
+public class lap139 {
+	 public static void main(String args[])throws Exception{  
+		  FileOutputStream fout1=new FileOutputStream("f1.txt");  
+		  FileOutputStream fout2=new FileOutputStream("f2.txt");  
+	  
+		  ByteArrayOutputStream bout=new ByteArrayOutputStream();  
+		  bout.write(65);  
+	  bout.writeTo(fout1);  
+		  bout.writeTo(fout2);  
+		  
+		  bout.flush();  
+		  bout.close();//has no effect  
+		  System.out.println("success...");  
+		 }  
+
+}
