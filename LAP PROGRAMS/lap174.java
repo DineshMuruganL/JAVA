7b7d1f19075364ff2be1c26c736525d7de930c0a
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-09-02 19:10:08 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap174.java
@@ -0,0 +1,26 @@
+//JTextField class:
+	import java.awt.Color;  
+	import javax.swing.*;  
+	  
+	public class lap174 {  
+    JTextArea area;  
+	    JFrame f;  
+    lap174(){  
+	    f=new JFrame();  
+	          
+	    area=new JTextArea(300,300);  
+	    area.setBounds(10,30,300,300);  
+	      
+	    area.setBackground(Color.black);  
+	    area.setForeground(Color.white);  
+	          
+	    f.add(area);  
+	      
+	    f.setSize(400,400);  
+	    f.setLayout(null);  
+	    f.setVisible(true);  
+	}  
+	    public static void main(String[] args) {  
+	        new lap174();  
+	    }  
+	}  
