6d7be6770a923c1d21d82b59603b96357c91ef2f
DineshMuruganL <40655889+DineshMuruganL@users.noreply.github.com>
2018-09-11 18:05:27 +0530

    Add files via upload

---
+++ LAP PROGRAMS/lap193.java
@@ -0,0 +1,24 @@
+//BoxLayout class with Y-AXIS
+	import java.awt.*;  
+	import javax.swing.*;  
+	  
+    public class lap193 extends Frame {  
+	 Button buttons[];  
+	  
+	 public lap193 () {  
+	   buttons = new Button [5];  
+	    
+	   for (int i = 0;i<5;i++) {  
+	      buttons[i] = new Button ("Button " + (i + 1));  
+	      add (buttons[i]);  
+	    }  
+	  
+	setLayout (new BoxLayout (this, BoxLayout.Y_AXIS));  
+	setSize(400,400);  
+	setVisible(true);  
+	}  
+	  
+	public static void main(String args[]){  
+lap193 b=new lap193();  
+}  
+	}  
